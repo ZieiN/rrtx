fc1934f362646c77aae56739cab7135e3c04fd4d
ZieiN <zainalabedeenali@hotmail.com>
2022-01-26 08:09:03 +0300

    Visualization

--- Visualization/vis_traj_difference.py
+++ Visualization/vis_traj_difference.py
@@ -12,7 +12,7 @@ for imageNm in range(1,4):
             for line in f:
                 a = [float(i) for i in line.split()]
                 if a[0]>=-0.5:
-                    diffs.append(a[0])
+                    diffs[imageNm-1].append(a[0])
             f.close
 
 
@@ -20,12 +20,13 @@ fig, ax = pl.subplots()
 pl.xlabel("Map", fontsize=20);
 pl.ylabel("Lock-step Euclidean Dist", fontsize=20)
 pl.title("Difference between consecutive trajectories for each map", fontsize=20)
-pl.violinplot(diffs)
+pl.boxplot(diffs)
+pl.plot([i/10 for i in range(5, 35)], [0.4 for i in range(5, 35)])
 F = pl.gcf()
 Size = F.get_size_inches()
 F.set_size_inches(Size[0] * 3, Size[1] * 3, forward=True)
 pl.tight_layout()
-pl.savefig('../output/'+'Planning times'+".png")
+pl.savefig('../output/'+'Metric values'+".png")
 pl.show()
 # input("hit[enter] to end.")
 pl.close('all')
